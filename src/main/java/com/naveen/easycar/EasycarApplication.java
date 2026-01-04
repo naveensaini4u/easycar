eee5c885e9ded3018df65342fac61ffc3e29c5a7
Naveen Saini <naveen.saini@anaplan.com>
2026-01-04 07:48:34 +0530

    initial setup

---
+++ src/main/java/com/naveen/easycar/EasycarApplication.java
@@ -0,0 +1,13 @@
+package com.naveen.easycar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasycarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasycarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/naveen/easycar/EasycarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.naveen.easycar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasycarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
